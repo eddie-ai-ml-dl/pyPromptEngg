f966dfec47349453619910534047aa17baa73136
eddie-ai-ml-dl <eddie.amadie@gmail.com>
2025-01-16 20:30:16 -0500

    Created new labs.

---
+++ src/gemini.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+load_dotenv()
+GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
+# print(GOOGLE_API_KEY)
+genai.configure(api_key=GOOGLE_API_KEY)
+
+DEF_MODEL = "models/gemini-2.0-flash-exp"  #'gemini-pro'
+
+def get_model(model_name=DEF_MODEL):
+    return genai.GenerativeModel(model_name)
+
+def invoke_model(model, prompt):
+    response=model.generate_content(prompt)
+    return response.text
