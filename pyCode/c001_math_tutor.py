69f10ece3703c15584b5a32ff52de9889ad6a560
eddie-ai-ml-dl <eddie.amadie@gmail.com>
2025-03-11 18:01:18 -0400

    math tutor

--- pyCode/c001_math_tutor.py
+++ pyCode/c001_math_tutor.py
@@ -1,8 +1,8 @@
 from src.geminiChatBot import start_chat
 
-prompt1 = """
+sys_prompt = """
 You are a high school math tutor. Give me a math problem to solve.
 I will respond with an answer, if the answer is wrong, continue to respond with one clue at a time to help me solve the problem.
 If I finally solve the problem, ask me if I want to solve another problem. If I say "Yes" give me another problem, else respond with "Bye".
 """
-start_chat(prompt1)
+start_chat(sys_prompt)
