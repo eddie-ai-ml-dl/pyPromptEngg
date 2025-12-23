e118924d7d48516c9b3771dc28233d5948b4088b
eddie-ai-ml-dl <Eddie.amadie@sju.edu>
2025-12-23 17:27:46 -0500

    tools and agents

--- src/y.py
+++ src/y.py
@@ -1,48 +1,18 @@
-from google import genai
-from google.genai import types
-import os
-from dotenv import load_dotenv
+from ddgs import DDGS
 
-load_dotenv()
+def dd_search(q, max_results=1):
+    try:
+        results=DDGS().text(q, max_results=max_results)
+        print("WEB SEARCH RESULTS>>>", results)
+        if results:
+            return results[0]["body"]
+        else:
+            return "No relevant information found."
+    except Exception as e:
+        print(f"Search error: {e}")
+        return "Error: Could not retrieve search results."
+    
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-prompt = "What's the weather like in London?"
-
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents=prompt)
-
-print(response.text)
-
-def get_current_weather(location: str):
-    """Gets the current weather conditions for a given location.
-
-    Args:
-        location: The city and state, e.g. San Francisco, CA
-
-    Returns:
-        A dictionary containing the temperature(e.g. 70), unit, feels_like (e.g. 65), condition(e.g. cloudy), humidity and wind_speed.
-    """
-
-    return {
-        "location": location,
-        "temperature": 61.0,
-        "unit": "fahrenheit",
-        "feels_like": 61.0,
-        "condition": "Partly cloudy",
-        "humidity": 30,
-        "wind_speed": "2.2 mph"
-    }
-
-config = types.GenerateContentConfig(
-    tools=[get_current_weather]
-)  # Pass the function itself
-
-# Make the request
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents=prompt,
-    config=config,
-)
-
-print("FUNCTION CALLING:", response.text)
+if __name__ == '__main__':
+    # Simple test
+    print(dd_search("population of France"))
