aac3687c419bfda2c933d80156a7413cbbb906eb
eddie-ai-ml-dl <eddie.amadie@gmail.com>
2026-04-10 22:56:50 -0400

    added new agents and governence demos.

--- lessons/05_Agents_and_Tools/domo_agents/openai_agents_testing.py
+++ lessons/05_Agents_and_Tools/domo_agents/openai_agents_testing.py
@@ -1,23 +1,26 @@
 # gemini_agent_run.py
 import asyncio
+from dotenv import load_dotenv
 from agents import Agent, Runner
+
+load_dotenv()
 
 Runner.trace_export_enabled = False
 
 async def main():
-    # Create Gemini agent
-    gemini_agent = Agent(
-        name="Gemini Agent",
+    # Create ChatGPT agent
+    agent = Agent(
+        name="ChatGPT Agent",
         instructions="""
-        You are a helpful assistant powered by Gemini.
+        You are a helpful assistant powered by ChatGPT.
         Be concise, factual, and clear in your responses.
         """,
-        model="litellm/gemini/gemini-2.5-flash"
+        model="gpt-4o-mini"
     )
 
     # Run the agent asynchronously
-    result = await Runner.run(gemini_agent, "Explain agentic AI in simple terms.")
-    print("=== Gemini Agent Output ===")
+    result = await Runner.run(agent, "Explain agentic AI in simple terms.")
+    print("=== ChatGPT Agent Output ===")
     print(result.final_output)
 
 if __name__ == "__main__":
